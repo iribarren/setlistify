37599b466053d6b8bb9fc7dcedd59e1e598337d8
Aritz Iribarren Galarza <iribarren.aritz@gmail.com>
2019-08-31 01:32:02 +0200

    added the show/hide song options in the SongTable and Song components

--- assets/js/components/Song.vue
+++ assets/js/components/Song.vue
@@ -1,30 +1,70 @@
 <template>
-    <tr>
+    <tr v-if="show">
         <td>{{song.id}}</td>
         <td>{{song.songNameSetlist}}</td>
         <td>{{song.songName }}</td>
         <td>{{song.album }}</td>
         <td><img :src="song.cover" /></td>
-        <td><input type="radio" :id="song.id" :name="'songs['+song.id+']'" :value="song.id" /></td>
+        <td><input type="radio" @click="selectSong" v-bind:id="song.id" v-bind:name="'songs['+song.id+']'" v-bind:value="song.id" v-bind:checked="selected" /></td>
+        <td><button class="btn btn-dark" @click.prevent="showMore" v-if="selected">{{button}}</button></a></td>
+
     </tr>
 </template>
 
 <script>
+    import { EventBus } from '../event-bus.js';
     export default {
         name: "song",
-        props: ['songinfo'],
+        props: ['songinfo', 'index'],
         data() {
             return {
-                song: this.songinfo
+                song: this.songinfo,
+                i: this.index,
+                show: false,
+                selected: false,
+                button: '+'
             }
         },
-        /*
-        computed: {
-            show () {
-                return this.songVersions.length>0
+        methods: {
+            showMore: function () {
+                if (this.button == '+') {
+                    this.button = '-'
+                    EventBus.$emit('show-all', this.song.id)
+                } else {
+                    this.button = '+'
+                    EventBus.$emit('hide-all', this.song.id)
+                }
+            },
+            selectSong: function() {
+                this.selected = true
+                this.button = '+'
+                EventBus.$emit('songSelected', {'id':this.song.id,'spotify_id': this.song.spotify_id})
             }
-        }
-        */
+        },
+        beforeMount() {
+            if (this.i == 0) {
+                this.show = true
+                this.selected = true
+            }
+            EventBus.$on('show-all', id => {
+                if (id == this.song.id) {
+                    this.show = true
+                }
+            });
+            EventBus.$on('hide-all', id => {
+                if (id == this.song.id) {
+                    this.show = this.selected
+                }
+            });
+            EventBus.$on('songSelected', data => {
+                if (data.id == this.song.id) {
+                    if (data.spotify_id != this.song.spotify_id) {
+                        this.selected = false;
+                    }
+                    this.show = this.selected
+                }
+            });
+        },
     }
 </script>
 
--- assets/js/components/SongTable.vue
+++ assets/js/components/SongTable.vue
@@ -8,10 +8,11 @@
                 <td>Album</td>
                 <td>Cover</td>
                 <td>Add</td>
+                <td>&nbsp;</td>
             </tr>
         </thead>
         <tbody v-for="(variations,index) in songs" v-bind:key="index">
-            <song v-for="song in variations" v-bind:key="song.spotify_id"  v-bind:songinfo="song"></song>
+            <song v-for="(song,i) in variations" v-bind:key="song.spotify_id"  v-bind:songinfo="song" v-bind:index="i"></song>
         </tbody>
     </table>    
 </template>
